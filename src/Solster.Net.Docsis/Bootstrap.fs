deff9850452c2bd4c1d366ab0c50cd9ba57f3e08
Paul Hughes <paul.hughes@solster.co.uk>
2016-12-22 14:20:58 +0100

    removed unused projects

--- src/Solster.Net.Docsis/Bootstrap.fs
+++
@@ -1,9 +0,0 @@
-﻿
-module Solster.Net.Bootstrap
-
-open System.Net
-open System.Net.Sockets
-
-let receive = 
-    use client = new UdpClient(IPEndPoint(IPAddress.Any, 0x44), EnableBroadcast = true, ExclusiveAddressUse = false)
-    fun () -> (client.ReceiveAsync() |> Async.AwaitTask |> Async.RunSynchronously)
